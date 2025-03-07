cb5650ed53214d3a8530b31893bd2e761d09a8a2
jbrecak <juraj.brecak@reversinglabs.com>
2025-03-07 05:58:02 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM rockylinux:8.9
+WORKDIR /opt/icapeg
+RUN dnf -y install golang git nginx squid vim-enhanced findutils procps iproute
+
+RUN git clone https://github.com/egirna/icapeg.git .
+RUN go build .
+
+RUN cp /opt/icapeg/ICAPeg-DS.pdf /usr/share/nginx/html/
+RUN cp /opt/icapeg/files-for-testing/sample.pdf /usr/share/nginx/html/
+
+COPY config.toml squid.conf test.bash .
+COPY static.conf /etc/nginx/conf.d/
+
+CMD /bin/bash -e test.bash
---
+++ test.bash
@@ -0,0 +1,37 @@
+# run NGINX to serve the PDF files
+/usr/sbin/nginx
+
+# run Squid
+/usr/sbin/squid -f /opt/icapeg/squid.conf
+
+# run ICAPeg
+./icapeg &
+
+echo "Testing download of small file (sample.pdf) directly from NGINX"
+SHA_SMALL_FILE=$(sha1sum /usr/share/nginx/html/sample.pdf | awk '{print $1}')
+curl --silent -o - http://127.0.0.1:9080/sample.pdf | sha1sum | grep "$SHA_SMALL_FILE" && echo "WORKS OK"
+echo
+echo "Testing download of large file (ICAPeg-DS.pdf) directly from NGINX"
+SHA_LARGE_FILE=$(sha1sum /usr/share/nginx/html/ICAPeg-DS.pdf | awk '{print $1}')
+curl --silent -o - http://127.0.0.1:9080/ICAPeg-DS.pdf | sha1sum | grep "$SHA_LARGE_FILE" && echo "WORKS OK"
+echo
+echo "Setting http_proxy to redirect traffic via Squid to ICAPeg /echo service"
+export http_proxy=127.0.0.1:3128
+echo
+echo "Testing download of small file (sample.pdf) from NGINX via Squid and ICAPeg /echo"
+SHA_SMALL_FILE=$(sha1sum /usr/share/nginx/html/sample.pdf | awk '{print $1}')
+curl --silent -o - http://127.0.0.1:9080/sample.pdf | sha1sum | grep "$SHA_SMALL_FILE" && echo "WORKS OK"
+echo
+echo "THE NEXT TEST GETS STUCK AND NEVER PRINTS 'WORKS OK'"
+echo
+echo "Testing download of large file (ICAPeg-DS.pdf) from NGINX via Squid and ICAPeg /echo"
+SHA_LARGE_FILE=$(sha1sum /usr/share/nginx/html/ICAPeg-DS.pdf | awk '{print $1}')
+curl --silent -o - http://127.0.0.1:9080/ICAPeg-DS.pdf | sha1sum | grep "$SHA_LARGE_FILE" && echo "WORKS OK"
+
+# NOTES:
+#
+# to stop squid run:
+#	pkill -f /usr/sbin/squid
+#
+# to view relevant logs run:
+#	tail -F /var/log/squid/*.log /var/log/nginx/* /opt/icapeg/logs/logs.json &
